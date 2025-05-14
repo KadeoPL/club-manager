6fe543758a907bde55b59b465481d038ccee0869
KadeoPL <stopkakrzysztof1@gmail.com>
2025-05-14 22:40:18 +0200

    add registery function

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,30 +1,40 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@/lib/generated/prisma";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
-  const { login, email, password } = await req.json();
+  try {
+    const { login, email, password } = await req.json();
 
-  const existingUser = await prisma.user.findUnique({
-    where: { OR: [{ email: email }, { login: login }] },
-  });
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { login }],
+      },
+    });
 
-  if (existingUser) {
-    return null;
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Użytkownik z takim loginem lub e-mailem już istnieje" },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        login,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error("API error", error);
+    return NextResponse.json({ error: "Registery error" }, { status: 500 });
   }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const user = await prisma.user.create({
-    data: {
-      login,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return NextResponse.json({ user });
 }
--- components/layout/RegisterForm.tsx
+++ components/layout/RegisterForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import AlertDestructive from "./AlertDestructive";
 
 const formSchema = z.object({
   login: z
@@ -47,71 +48,88 @@ export default function RegisterForm() {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const res = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.ok) {
+      console.log("ok");
+    } else {
+      const data = await res.json();
+      console.error(data.error || "Error registering");
+      setError(data.error);
+    }
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-4 min-w-[300px] w-1/4"
-      >
-        <FormField
-          control={form.control}
-          name="login"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Login</FormLabel>
-              <FormControl>
-                <Input
-                  className="text-xs md:text-sm"
-                  placeholder="Wprowadź nazwę użytkownika"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>E-mail</FormLabel>
-              <FormControl>
-                <Input
-                  className="text-xs md:text-sm"
-                  placeholder="Wprowadź swój adres e-mail"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Hasło</FormLabel>
-              <FormControl>
-                <Input
-                  className="text-xs md:text-sm"
-                  placeholder="Wybierz silne hasło (min. 8 znaków, cyfry i litery)"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="w-full">
-          Wyślij
-        </Button>
-      </form>
-    </Form>
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 min-w-[300px] w-1/4"
+        >
+          <FormField
+            control={form.control}
+            name="login"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Login</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-xs md:text-sm"
+                    placeholder="Wprowadź nazwę użytkownika"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-xs md:text-sm"
+                    placeholder="Wprowadź swój adres e-mail"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Hasło</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-xs md:text-sm"
+                    placeholder="Wybierz silne hasło (min. 8 znaków, cyfry i litery)"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            Wyślij
+          </Button>
+        </form>
+      </Form>
+      {error && <AlertDestructive title={"Błąd"} text={error} />}
+    </div>
   );
 }
