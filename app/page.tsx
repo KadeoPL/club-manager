a380bffd8bc7c59a9c37a39b26c22b3b24ac9b39
KadeoPL <stopkakrzysztof1@gmail.com>
2025-05-19 15:05:01 +0200

    Change Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/components/layout/navbar";
+import Navbar from "@/components/layout/Navbar";
 export default function Home() {
   return (
     <div className="w-full min-h-max">
