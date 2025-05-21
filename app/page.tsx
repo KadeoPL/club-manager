9f91d64780a067d262e44711df634d9be55e21e9
KadeoPL <stopkakrzysztof1@gmail.com>
2025-05-21 23:02:40 +0200

    create herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Navbar from "@/components/layout/Navbar";
+import HeroSection from "@/components/layout/HeroSection";
+
 export default function Home() {
   return (
     <div className="w-full min-h-max">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ components/layout/HeroSection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+export default function HeroSection() {
+  return (
+    <div className="w-full h-[720px] bg-red-500 ">
+      <div className="w-full h-full flex justify-center bg-gradient-to-t from-black/70 to-black/0">
+        <div className="w-full flex justify-center items-end max-w-6xl mb-16 ">
+          <div className="w-2/3 text-4xl font-bold text-white">
+            Trener Wyroba o mijającej rundzie i okresie przygotowawczym
+          </div>
+          <div className="w-1/3 flex justify-end gap-2">
+            <Button className="w-16 h-16 p-0 flex items-center justify-center">
+              <ChevronLeft style={{ width: "24px", height: "24px" }} />
+            </Button>
+            <Button className="w-16 h-16">
+              <ChevronRight style={{ width: "24px", height: "24px" }} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
