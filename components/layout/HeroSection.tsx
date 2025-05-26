7cfa14803927998b5567931014848ded187d7beb
KadeoPL <stopkakrzysztof1@gmail.com>
2025-05-26 22:56:00 +0200

    add functions to button in HeroSection

--- components/layout/HeroSection.tsx
+++ components/layout/HeroSection.tsx
@@ -1,11 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import HeroSectionSlide from "../ui/heroSectionSlide";
 
+const articles = [
+  {
+    text: "Trener Wyroba o mijającej rundzie i okresie przygotowawczym",
+    img: "",
+    url: "",
+  },
+  {
+    text: "Jakiś inny tytuł tutaj",
+    img: "",
+    url: "",
+  },
+  {
+    text: "Jeszcze jakiś inny tytuł tutaj",
+    img: "",
+    url: "",
+  },
+];
+
 export default function HeroSection() {
+  const [activeArticle, setActiveArticle] = useState<number>(0);
+
+  const handleClick = (direction: "prev" | "next") => {
+    if (direction === "next") {
+      setActiveArticle((prevArticlesIndex) =>
+        prevArticlesIndex === articles.length - 1 ? 0 : prevArticlesIndex + 1
+      );
+    } else {
+      setActiveArticle((prevArticlesIndex) =>
+        prevArticlesIndex === 0 ? articles.length - 1 : prevArticlesIndex - 1
+      );
+    }
+  };
+
   return (
     <HeroSectionSlide
-      text="Trener Wyroba o mijającej rundzie i okresie przygotowawczym"
-      img=""
-      url=""
+      text={articles[activeArticle].text}
+      img={articles[activeArticle].img}
+      url={articles[activeArticle].url}
+      onClick={handleClick}
     />
   );
 }
--- components/ui/heroSectionSlide.tsx
+++ components/ui/heroSectionSlide.tsx
@@ -6,19 +6,28 @@ interface Props {
   text: string;
   img: string;
   url: string;
+  onClick: (direction: "prev" | "next") => void;
 }
 
-export default function HeroSectionSlide({ text, img, url }: Props) {
+export default function HeroSectionSlide({ text, img, url, onClick }: Props) {
   return (
     <div className="w-full h-[720px] bg-red-500 ">
       <div className="w-full h-full flex justify-center bg-gradient-to-t from-black/70 to-black/0">
-        <div className="w-full flex justify-center items-end max-w-6xl mb-16 ">
-          <div className="w-2/3 text-4xl font-bold text-white">{text}</div>
-          <div className="w-1/3 flex justify-end gap-2">
-            <Button className="w-16 h-16 p-0 flex items-center justify-center">
+        <div className="w-full flex flex-col md:flex-row md:justify-center md:items-end justify-end max-w-6xl mb-16 px-10 gap-10">
+          <div className="w-full md:w-2/3 text-4xl font-bold text-white">
+            {text}
+          </div>
+          <div className="w-full md:w-1/3 flex justify-start md:justify-end gap-2">
+            <Button
+              className="w-16 h-16 p-0 flex items-center justify-center cursor-pointer"
+              onClick={() => onClick("prev")}
+            >
               <ChevronLeft style={{ width: "24px", height: "24px" }} />
             </Button>
-            <Button className="w-16 h-16">
+            <Button
+              className="w-16 h-16 cursor-pointer"
+              onClick={() => onClick("next")}
+            >
               <ChevronRight style={{ width: "24px", height: "24px" }} />
             </Button>
           </div>
