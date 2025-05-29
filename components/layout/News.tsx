c08bfac59a22765e6ac7d3f18f61842e88866ca1
KadeoPL <stopkakrzysztof1@gmail.com>
2025-05-29 22:25:17 +0200

    add article box

--- components/layout/News.tsx
+++ components/layout/News.tsx
@@ -3,10 +3,15 @@ import ArticleBox from "../ui/ArticleBox";
 
 export default function News() {
   return (
-    <div className="max-w-6xl flex mx-auto gap-4">
-      <ArticleBox />
-      <ArticleBox />
-      <ArticleBox />
+    <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16">
+      <div>
+        <h1 className="text-3xl font-bold">Ostatnie aktualności</h1>
+      </div>
+      <div className="flex gap-4">
+        <ArticleBox />
+        <ArticleBox />
+        <ArticleBox />
+      </div>
     </div>
   );
 }
--- components/ui/ArticleBox.tsx
+++ components/ui/ArticleBox.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function ArticleBox() {
-  return <div className="w-full bg-amber-200">ArticleBox</div>;
+  return (
+    <div className="w-full flex flex-col gap-2">
+      <div className="h-60 bg-red-300"></div>
+      <div>
+        <p className="uppercase text-sm">Kategoria</p>
+      </div>
+      <div>
+        <h1 className="text-xl font-bold">Tytuł jakiś tutaj będzie</h1>
+      </div>
+      <div>
+        <p className="text-xs text-gray-400">29.05.2025</p>
+      </div>
+    </div>
+  );
 }
