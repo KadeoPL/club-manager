4107140f2d07ee0adedb5cdba558f985c55ea9b6
KadeoPL <stopkakrzysztof1@gmail.com>
2025-06-04 21:12:19 +0200

    fix mobile view news section

--- components/layout/News.tsx
+++ components/layout/News.tsx
@@ -3,11 +3,11 @@ import ArticleBox from "../ui/ArticleBox";
 
 export default function News() {
   return (
-    <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16">
+    <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16 px-10">
       <div>
         <h1 className="text-3xl font-bold">Ostatnie aktualności</h1>
       </div>
-      <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row gap-10 md:gap-4">
         <ArticleBox />
         <ArticleBox />
         <ArticleBox />
