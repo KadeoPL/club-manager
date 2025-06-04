78271b0b6c8102758e6e9fed4e97eb495df37c44
KadeoPL <stopkakrzysztof1@gmail.com>
2025-06-04 21:13:56 +0200

    fix mobile view on news section

--- components/layout/News.tsx
+++ components/layout/News.tsx
@@ -12,6 +12,22 @@ export default function News() {
         <ArticleBox />
         <ArticleBox />
         <ArticleBox />
+=======
+    <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16 px-6">
+      <div>
+        <h1 className="text-3xl font-bold">Ostatnie aktualności</h1>
+      </div>
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2">
+          <ArticleBox isFirst={true} />
+        </div>
+        <div className="w-full md:w-1/2 grid grid-cols-2 gap-4">
+          <ArticleBox />
+          <ArticleBox />
+          <ArticleBox />
+          <ArticleBox />
+        </div>
+>>>>>>> 91ea363ef3e453bcf31af31be1c57a466105a718
       </div>
     </div>
   );
