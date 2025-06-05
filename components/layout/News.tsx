5274d7dff649579f8cd5b65e55f748d7ce077b23
KadeoPL <stopkakrzysztof1@gmail.com>
2025-06-05 22:06:29 +0200

    merge

--- components/layout/News.tsx
+++ components/layout/News.tsx
@@ -3,16 +3,6 @@ import ArticleBox from "../ui/ArticleBox";
 
 export default function News() {
   return (
-<<<<<<< HEAD
-    <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16 px-10">
-      <div>
-        <h1 className="text-3xl font-bold">Ostatnie aktualności</h1>
-      </div>
-      <div className="flex flex-col md:flex-row gap-10 md:gap-4">
-        <ArticleBox />
-        <ArticleBox />
-        <ArticleBox />
-=======
     <div className="max-w-6xl flex flex-col mx-auto gap-4 my-16 px-6">
       <div>
         <h1 className="text-3xl font-bold">Ostatnie aktualności</h1>
@@ -27,7 +17,6 @@ export default function News() {
           <ArticleBox />
           <ArticleBox />
         </div>
->>>>>>> 91ea363ef3e453bcf31af31be1c57a466105a718
       </div>
     </div>
   );
