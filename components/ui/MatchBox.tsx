92d8db5ead53e5d97bd10348ef8f97e2880270e9
KadeoPL <stopkakrzysztof1@gmail.com>
2025-06-06 15:43:15 +0200

    add animation to club logo on match box

--- components/ui/MatchBox.tsx
+++ components/ui/MatchBox.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+"use client";
+
+import { useRef } from "react";
 import Image from "next/image";
+import { motion, useInView } from "framer-motion";
 
 interface Props {
   isEnd?: boolean;
 }
 
 export default function MatchBox({ isEnd }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false, amount: 0.5 });
   return (
     <div className="bg-white p-6 flex flex-col justify-center items-center">
       <div className=" py-2 px-4 bg-primary text-white text-sm">
@@ -13,14 +18,25 @@ export default function MatchBox({ isEnd }: Props) {
       </div>
       <div className="flex w-full justify-around items-center mt-6">
         <div className="flex flex-col">
-          <div className="relative h-16">
+          <motion.div
+            ref={ref}
+            className="relative h-16"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={
+              isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }
+            }
+            transition={{
+              duration: 0.8,
+              scale: { type: "spring", bounce: 0.5 },
+            }}
+          >
             <Image
               src="/images/gdovia_logo.png"
               alt="Gdovia Gdów logo"
               fill
               style={{ objectFit: "contain" }}
             />
-          </div>
+          </motion.div>
           <div className="font-bold text-md mt-2 text-center">Gdovia Gdów</div>
         </div>
         <div className="flex text-4xl font-bold gap-6 items-center px-6">
@@ -37,14 +53,25 @@ export default function MatchBox({ isEnd }: Props) {
           )}
         </div>
         <div className="flex flex-col">
-          <div className="relative h-16">
+          <motion.div
+            ref={ref}
+            className="relative h-16"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={
+              isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }
+            }
+            transition={{
+              duration: 0.8,
+              scale: { type: "spring", bounce: 0.5 },
+            }}
+          >
             <Image
               src="/images/gdovia_logo.png"
               alt="Gdovia Gdów logo"
               fill
               style={{ objectFit: "contain" }}
             />
-          </div>
+          </motion.div>
           <div className="font-bold text-md mt-2 text-center">Gdovia Gdów</div>
         </div>
       </div>
