9af7fd1fba94e54dba6153312c3790419bc5485d
KadeoPL <stopkakrzysztof1@gmail.com>
2025-08-08 08:47:16 +0200

    add render rich text

--- app/artykuly/[slug]/page.tsx
+++ app/artykuly/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import { getArticleBySlug } from "@/lib/api/api";
-import {
-  BlocksRenderer,
-  type BlocksContent,
-} from "@strapi/blocks-react-renderer";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import Image from "next/image";
+
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 export default async function Page({
   params,
@@ -15,7 +15,44 @@ export default async function Page({
   if (!article) {
     return <div>Nie znaleziono artykułu</div>;
   }
-  article.content.map((index: number) => {
-    return <BlocksRenderer content={article.content[index]} />;
-  });
+
+  return (
+    <div className="max-w-6xl mx-auto px-10">
+      <h1 className="text-5xl font-bold w-4xl  mx-auto py-10">
+        {article.title}
+      </h1>
+      <div className="relative w-full h-100">
+        <Image
+          src={STRAPI_URL + article.coverImage.url}
+          fill
+          alt={article.coverImage.alternativeText}
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      {article.content.map((block: any, index: number) => {
+        switch (block.__component) {
+          case "text-block.text-block":
+            return <BlocksRenderer key={index} content={block.text} />;
+
+          case "image-block.image-block":
+            return (
+              <div key={index}>
+                {block.image?.url && (
+                  <Image
+                    src={STRAPI_URL + block.image.url}
+                    alt={block.image.alternativeText || ""}
+                    height={200}
+                    width={200}
+                  />
+                )}
+              </div>
+            );
+
+          default:
+            return null;
+        }
+      })}
+    </div>
+  );
 }
