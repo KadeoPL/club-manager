89543b74f681223e2fd99767868f6f580d2c2b1d
KadeoPL <stopkakrzysztof1@gmail.com>
2025-10-16 13:38:19 +0200

    fix route for teams

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -4,6 +4,17 @@ export async function POST(req: Request) {
   const data = await req.json();
   const name = data.name;
   try {
+    const result = await pool.query(`SELECT 1 FROM teams WHERE name = $1`, [
+      name,
+    ]);
+
+    if (result.rows.length > 0) {
+      return Response.json(
+        { message: `Drużyna o tej nazwie już istnieje.` },
+        { status: 409 }
+      );
+    }
+
     await pool.query("INSERT INTO teams (name) VALUES ($1)", [name]);
   } catch (error) {
     return Response.json(
@@ -15,4 +26,50 @@ export async function POST(req: Request) {
     success: true,
     message: ` ${name} zostały dodany.`,
   });
+}
+
+export async function GETALL(position: number) {
+  const limit = 10;
+  const offset = position;
+  try {
+    const result = await pool.query(`SELECT * FROM teams LIMIT $1 OFFSET $2`, [
+      limit,
+      offset,
+    ]);
+    return Response.json(result.rows);
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Failed to fetch teams",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(name: string) {
+  try {
+    const result = await pool.query(
+      "SELECT name * FROM teams WHERE name = $1",
+      [name]
+    );
+
+    if (result.rows.length === 0) {
+      return Response.json(
+        {
+          message: "Nie znaleziono drużyny",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(result.rows[0]);
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Failed to fetch team",
+      },
+      { status: 500 }
+    );
+  }
 }
